baa114ac873e7040484660244ab6ab61bb49a7fc
oezcelik55 <oezcelikmete@gmail.com>
2026-02-17 14:53:51 +0300

    Fix sum3 and reach 10/10 pylint score

--- sum_of_three.py
+++ sum_of_three.py
@@ -4,4 +4,5 @@ def sum3(first_number, second_number, third_number):
     """
     Takes three numbers and returns the sum of those numbers
     """
-    pass  # YOUR CODE HERE
+    # Buradaki pass satırını sildik ve yerine return ekledik:
+    return first_number + second_number + third_number
